a4e651135164336e6442e76ffd137265c4581555
Wyatt <cadewya@tamu.edu>
2024-12-07 23:06:10 -0600

    Copy ghidra script to /home/wyatt/ghidra_scripts as part of installation

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "If you wish to install the ghidra script, it will be installed to $HOME/ghidra_scripts"
+echo "Install ghidra script? (Y/N)"
+read answer
+
+if [ "$answer" = "Y" ]; then
+	cp ghidra_script.py $HOME/ghidra_scripts/PatchPal.py
+fi
+
 echo -e "#!/usr/bin/python3\n" > patchpal
 chmod a+x patchpal
 cat patch_pal.py >> patchpal
